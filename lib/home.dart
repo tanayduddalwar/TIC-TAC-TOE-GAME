53c5110d88c6b52eb0fee566d3cceeb166bb777e
helping_wizard <121712960+helpingwizard@users.noreply.github.com>
2023-10-21 20:27:48 +0530

    Updated home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -3,6 +3,8 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+
+
 class GameScreen extends StatefulWidget {
   const GameScreen({Key? key}) : super(key: key);
 
@@ -207,6 +209,322 @@ class _GameScreenState extends State<GameScreen> {
     }
 
 
+
+    // Check for a draw
+    if (!winnerFound && filledBoxes == 9) {
+      setState(() {
+        resultDeclaration = 'Nobody Wins!';
+      });
+    }
+  }
+
+  void _updateScore(String winner) {
+    if (winner == 'O') {
+      oScore++;
+    } else if (winner == 'X') {
+      xScore++;
+    }
+    winnerFound = true;
+  }
+
+  void _clearBoard() {
+    setState(() {
+      for (int i = 0; i < 9; i++) {
+        displayXO[i] = '';
+      }
+      resultDeclaration = '';
+      matchedIndexes = [];
+    });
+    filledBoxes = 0;
+  }
+
+  Widget _buildTimer() {
+    final isRunning = timer == null ? false : timer!.isActive;
+
+    return isRunning
+        ? SizedBox(
+            width: 100,
+            height: 100,
+            child: Stack(
+              fit: StackFit.expand,
+              children: [
+                CircularProgressIndicator(
+                  value: 1 - seconds / maxSeconds,
+                  valueColor: AlwaysStoppedAnimation(Colors.white),
+                  strokeWidth: 8,
+                  backgroundColor: Colors.deepPurpleAccent,
+                ),
+                Center(
+                  child: Text(
+                    '$seconds',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 50,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        : Column(
+            children: [
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  padding: EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+                ),
+                onPressed: () {
+                  startTimer();
+                  _clearBoard();
+                  attempts++;
+                },
+                child: Text(
+                  attempts == 0 ? 'Start' : 'Play Again!',
+                  style: TextStyle(fontSize: 20, color: Colors.black),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    oScore = 0;
+                    xScore = 0;
+                  });
+                  _clearBoard(); // Optionally clear the board when resetting scores
+                },
+                child: Text("CLEAR"),
+              ),
+            ],
+          );
+  }
+}
+
+class GameScreen extends StatefulWidget {
+  const GameScreen({Key? key}) : super(key: key);
+
+  @override
+  State<GameScreen> createState() => _GameScreenState();
+}
+
+class _GameScreenState extends State<GameScreen> {
+  bool oTurn = true;
+  List<String> displayXO = ['', '', '', '', '', '', '', '', ''];
+  List<int> matchedIndexes = [];
+  int attempts = 0;
+
+  int oScore = 0;
+  int xScore = 0;
+  int filledBoxes = 0;
+  String resultDeclaration = '';
+  bool winnerFound = false;
+  static const maxSeconds = 30;
+  int seconds = maxSeconds;
+  Timer? timer;
+
+  static var customFontWhite = GoogleFonts.coiny(
+    textStyle: TextStyle(
+      color: Colors.white,
+      letterSpacing: 3,
+      fontSize: 28,
+    ),
+  );
+
+  void startTimer() {
+    timer = Timer.periodic(Duration(seconds: 1), (_) {
+      setState(() {
+        if (seconds > 0) {
+          seconds--;
+        } else {
+          stopTimer();
+        }
+      });
+    });
+  }
+
+  void stopTimer() {
+    resetTimer();
+    timer?.cancel();
+  }
+
+  void resetTimer() => seconds = maxSeconds;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurpleAccent,
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          'Player O',
+                          style: customFontWhite,
+                        ),
+                        Text(
+                          oScore.toString(),
+                          style: customFontWhite,
+                        ),
+                      ],
+                    ),
+                    SizedBox(width: 20),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          'Player X',
+                          style: customFontWhite,
+                        ),
+                        Text(
+                          xScore.toString(),
+                          style: customFontWhite,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: GridView.builder(
+                itemCount: 9,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                ),
+                itemBuilder: (BuildContext context, int index) {
+                  return GestureDetector(
+                    onTap: () {
+                      _tapped(index);
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        border: Border.all(
+                          width: 5,
+                          color: Colors.black,
+                        ),
+                        color: matchedIndexes.contains(index)
+                            ? Colors.yellow
+                            : Colors.pink,
+                      ),
+                      child: Center(
+                        child: Text(
+                          displayXO[index],
+                          style: TextStyle(
+                            fontSize: 64,
+                            color: matchedIndexes.contains(index)
+                                ? Colors.deepPurpleAccent
+                                : Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(resultDeclaration, style: customFontWhite),
+                    SizedBox(height: 10),
+                    _buildTimer(),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _tapped(int index) {
+    final isRunning = timer == null ? false : timer!.isActive;
+
+    if (isRunning) {
+      setState(() {
+        if (oTurn && displayXO[index] == '') {
+          displayXO[index] = 'O';
+
+          filledBoxes++;
+        } else if (!oTurn && displayXO[index] == '') {
+          displayXO[index] = 'X';
+          filledBoxes++;
+        }
+
+        oTurn = !oTurn;
+        _checkWinner();
+      });
+    }
+  }
+
+  void _checkWinner() {
+    // Check rows
+    for (int i = 0; i < 9; i += 3) {
+      if (displayXO[i] == displayXO[i + 1] &&
+          displayXO[i] == displayXO[i + 2] &&
+          displayXO[i] != '') {
+        setState(() {
+          resultDeclaration = 'Player ' + displayXO[i] + ' Wins!';
+          matchedIndexes.addAll([i, i + 1, i + 2]);
+          stopTimer();
+          _updateScore(displayXO[i]);
+        });
+        return;
+      }
+    }
+
+    // Check columns
+    for (int i = 0; i < 3; i++) {
+      if (displayXO[i] == displayXO[i + 3] &&
+          displayXO[i] == displayXO[i + 6] &&
+          displayXO[i] != '') {
+        setState(() {
+          resultDeclaration = 'Player ' + displayXO[i] + ' Wins!';
+          matchedIndexes.addAll([i, i + 3, i + 6]);
+          stopTimer();
+          _updateScore(displayXO[i]);
+        });
+        return;
+      }
+    }
+
+    // Check diagonals
+    if (displayXO[0] == displayXO[4] &&
+        displayXO[0] == displayXO[8] &&
+        displayXO[0] != '') {
+      setState(() {
+        resultDeclaration = 'Player ' + displayXO[0] + ' Wins!';
+        matchedIndexes.addAll([0, 4, 8]);
+        stopTimer();
+        _updateScore(displayXO[0]);
+      });
+      return;
+    }
+    if (displayXO[2] == displayXO[4] &&
+        displayXO[2] == displayXO[6] &&
+        displayXO[2] != '') {
+      setState(() {
+        resultDeclaration = 'Player ' + displayXO[2] + ' Wins!';
+        matchedIndexes.addAll([2, 4, 6]);
+        stopTimer();
+        _updateScore(displayXO[2]);
+      });
+      return;
+    }
 
     // Check for a draw
     if (!winnerFound && filledBoxes == 9) {
